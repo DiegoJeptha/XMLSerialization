57d34a14975a9acfbf098fd2ba6f1f9322394424
Diego Jeptha <diegotgjeptha@gmail.com>
2019-05-13 08:36:51 +0200

    Initial Commit

---
+++ XMLSerialization/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XMLSerialization
+{
+      class Program
+      {
+            static void Main(string[] args)
+            {
+                  Console.WriteLine("Hello World!");
+            }
+      }
+}
